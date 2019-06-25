ea6b85afb970335c494808bbd23f71ceb9a95f93
Leon <yanglyu.leon.7@gmail.com>
2019-06-25 00:40:06 -0700

    fea: Declaring the interface

--- app/src/main/java/com/idiotleon/tutorialretrofit2/WikiApiService.kt
+++ app/src/main/java/com/idiotleon/tutorialretrofit2/WikiApiService.kt
@@ -1,4 +1,16 @@
 package com.idiotleon.tutorialretrofit2
 
+import android.database.Observable
+import android.graphics.ColorSpace
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WikiApiService {
+
+    @GET("api.php")
+    fun hitCountCheck(@Query("action") action:String,
+                      @Query("format") format:String,
+                      @Query("list") list:String,
+                      @Query("srsearch")srsearch:String):Observable<Model.Result>
+
 }
