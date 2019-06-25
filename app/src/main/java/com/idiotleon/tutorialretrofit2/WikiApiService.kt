00f58c6b2ed4df46e9f236f43366936650d40081
Leon <yanglyu.leon.7@gmail.com>
2019-06-25 10:35:51 -0700

    tutorial: Declaring the service

--- app/src/main/java/com/idiotleon/tutorialretrofit2/WikiApiService.kt
+++ app/src/main/java/com/idiotleon/tutorialretrofit2/WikiApiService.kt
@@ -2,15 +2,33 @@ package com.idiotleon.tutorialretrofit2
 
 import android.database.Observable
 import android.graphics.ColorSpace
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WikiApiService {
 
     @GET("api.php")
-    fun hitCountCheck(@Query("action") action:String,
-                      @Query("format") format:String,
-                      @Query("list") list:String,
-                      @Query("srsearch")srsearch:String):Observable<Model.Result>
+    fun hitCountCheck(
+        @Query("action") action: String,
+        @Query("format") format: String,
+        @Query("list") list: String,
+        @Query("srsearch") srsearch: String
+    ): Observable<Model.Result>
 
+    companion object {
+        val BASE_URL = "https://en.wikipedia.org"
+        fun create(): WikiApiService {
+
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(WikiApiService::class.java)
+        }
+    }
 }
